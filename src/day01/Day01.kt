88b6059baa10869427990f3eca50e9e4438caa4b
dkoval <koval.dmitriy@gmail.com>
2023-11-27 19:46:46 +0100

    Getting ready

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,3 +1,10 @@
+package day01
+
+import println
+import readInput
+
+private const val DAY_ID = "01"
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -8,10 +15,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day$DAY_ID/Day${DAY_ID}_test")
+    check(part1(testInput) == 42)
+    //check(part2(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("day$DAY_ID/Day$DAY_ID")
     part1(input).println()
     part2(input).println()
 }
