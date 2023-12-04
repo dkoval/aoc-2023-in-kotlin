cd9578dab79f817ecc92f88d56f0b194b7f256ae
dkoval <koval.dmitriy@gmail.com>
2023-12-04 07:40:37 +0100

    Make println() extension function return original value

--- src/Utils.kt
+++ src/Utils.kt
@@ -17,11 +17,11 @@ fun readInputAsString(name: String): String = Path("src/$name.txt").readText()
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
-fun Any?.println() = println(this)
+fun <T> T.println(): T = this.also { println(it) }
