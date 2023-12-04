a42a93698d64cd091778b38e8f363617de9fece5
dkoval <koval.dmitriy@gmail.com>
2023-12-04 07:23:24 +0100

    Reduce code duplication

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -7,45 +7,47 @@ import java.util.Queue
 
 private const val DAY_ID = "04"
 
+private data class Card(val id: Int, val xs: Set<Int>, val ys: Set<Int>) {
+    val matches: Int = xs.count { it in ys }
+
+    companion object {
+        // Format:
+        // Card 1: 41 48 83 86 17 | 83 86  6 31 17 9 48 53
+        fun parseInput(input: List<String>): Map<Int, Card> {
+            fun parseNumbers(nums: String): Set<Int> = nums.split(" ").asSequence()
+                .filter { it.isNotEmpty() }
+                .map { it.trim().toInt() }
+                .toSet()
+
+            // card ID -> Card
+            return input.asSequence()
+                .map { line ->
+                    val (s1, s2) = line.split(": ")
+                    val (nums1, nums2) = s2.split(" | ")
+
+                    val id = s1.removePrefix("Card ").trim().toInt()
+                    val xs = parseNumbers(nums1)
+                    val ys = parseNumbers(nums2)
+
+                    id to Card(id, xs, ys)
+                }
+                .toMap()
+        }
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        // Card 1: 41 48 83 86 17 | 83 86  6 31 17 9 48 53
-        return input.asSequence()
-            .map { line ->
-                val (_, s2) = line.split(": ")
-                val (nums1, nums2) = s2.split(" | ")
-
-                val xs = nums1.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim().toInt() }.toSet()
-                val ys = nums2.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim().toInt() }.toSet()
-
-                xs to ys
-            }
-            .map { (xs, ys) ->
-                val matches = xs.count { it in ys }
+        return Card.parseInput(input).asSequence()
+            .map { (_, card) ->
+                val matches = card.matches
                 if (matches > 0) 1 shl (matches - 1) else 0
             }
             .sum()
     }
 
-    data class Card(val id: Int, val xs: Set<Int>, val ys: Set<Int>) {
-        val matches: Int by lazy { xs.count { it in ys } }
-    }
-
     fun part2(input: List<String>): Int {
-        // Card ID -> Card
-        val cards = input.asSequence()
-            .map { line ->
-                val (s1, s2) = line.split(": ")
-                val (nums1, nums2) = s2.split(" | ")
-
-                val id = s1.removePrefix("Card ").trim().toInt()
-                val xs = nums1.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim().toInt() }.toSet()
-                val ys = nums2.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim().toInt() }.toSet()
-
-                id to Card(id, xs, ys)
-            }
-            .toMap()
-
+        val cards = Card.parseInput(input)
         // cards to process
         val q: Queue<Int> = ArrayDeque(cards.keys)
         // card ID -> count
@@ -53,7 +55,7 @@ fun main() {
         while (!q.isEmpty()) {
             val id = q.poll()
             counts[id] = (counts[id] ?: 0) + 1
-
+            // generate IDs of next cards to process
             val matches = cards[id]!!.matches
             for (nextId in (id + 1)..(id + matches).coerceAtMost(cards.size)) {
                 q.offer(nextId)
