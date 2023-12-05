e7aa54bb9a1e994490cb50115dfaa7e1e7581434
dkoval <koval.dmitriy@gmail.com>
2023-12-05 19:08:25 +0100

    Structured input for Day05

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -6,41 +6,41 @@ import readInputAsString
 private const val DAY_ID = "05"
 
 fun main() {
-    data class MappingRange(val srcStart: Long, val dstStart: Long, val length: Long)
-    data class Input(val seeds: List<Long>, val mappings: Map<String, List<MappingRange>>)
+    data class Segment(val src: Long, val dst: Long, val length: Long)
+    data class Mapping(val id: String, val segments: List<Segment>)
+    data class Input(val seeds: List<Long>, val mappings: List<Mapping>)
 
     fun parseInput(input: String): Input {
-        //seeds: 79 14 55 13
+        // seeds: 79 14 55 13
         //
-        //seed-to-soil map:
-        //50 98 2
-        //52 50 48
+        // seed-to-soil map:
+        // 50 98 2
+        // 52 50 48
         //
-        //soil-to-fertilizer map:
-        //0 15 37
-        //37 52 2
-        //39 0 15
-        //...
+        // soil-to-fertilizer map:
+        // 0 15 37
+        // 37 52 2
+        // 39 0 15
+        // ...
         val lines = input.split("\n\n")
         val seeds = lines.first().removePrefix("seeds: ").split(" ").map { it.toLong() }
 
-        val mappings = lines.drop(1).asSequence()
+        val mappings = lines.drop(1)
             .map { block ->
-                //seed-to-soil map:
-                //50 98 2
-                //52 50 48
+                // seed-to-soil map:
+                // 50 98 2
+                // 52 50 48
                 val mapping = block.split("\n")
-                val (category, _) = mapping.first().split(" ")
+                val (id, _) = mapping.first().split(" ")
 
-                val ranges = mapping.drop(1)
+                val segments = mapping.drop(1)
                     .map { nums ->
-                        val (dstRangeStart, srcRangeStart, length) = nums.split(" ").map { it.toLong() }
-                        MappingRange(srcRangeStart, dstRangeStart, length)
+                        val (dst, src, length) = nums.split(" ").map { it.toLong() }
+                        Segment(src, dst, length)
                     }
 
-                category to ranges
+                Mapping(id, segments)
             }
-            .toMap()
 
         return Input(seeds, mappings)
     }
@@ -50,11 +50,11 @@ fun main() {
         return seeds.asSequence()
             .map { seed ->
                 var x = seed
-                for ((_, ranges) in mappings) {
-                    for (range in ranges) {
-                        if (x in range.srcStart until range.srcStart + range.length) {
-                            val offset = x - range.srcStart
-                            x = range.dstStart + offset
+                for (mapping in mappings) {
+                    for (segment in mapping.segments) {
+                        if (x in segment.src until segment.src + segment.length) {
+                            val offset = x - segment.src
+                            x = segment.dst + offset
                             break
                         }
                     }
@@ -66,12 +66,10 @@ fun main() {
 
     fun part2(input: String): Long {
         val (seeds, mappings) = parseInput(input)
-
         val rangeOfSeeds = seeds.chunked(2)
-        val categories = mappings.keys.reversed()
 
-        fun isGood(x: Long, categoryIndex: Int): Boolean {
-            if (categoryIndex == categories.size) {
+        fun isGood(x: Long, mappingIndex: Int): Boolean {
+            if (mappingIndex < 0) {
                 // check if x is in range of seeds
                 for ((start, length) in rangeOfSeeds) {
                     if (x in start until start + length) {
@@ -81,30 +79,29 @@ fun main() {
                 return false
             }
 
-            // x is the destination in the current category
-            val category = categories[categoryIndex]
+            // x is the destination in the current mapping
             var next = x
-            for (range in mappings[category]!!) {
-                if (x in range.dstStart until range.dstStart + range.length) {
-                    val offset = x - range.dstStart
-                    next = range.srcStart + offset
+            for (segment in mappings[mappingIndex].segments) {
+                if (x in segment.dst until segment.dst + segment.length) {
+                    val offset = x - segment.dst
+                    next = segment.src + offset
                     break
                 }
             }
-            return isGood(next, categoryIndex + 1)
+            return isGood(next, mappingIndex - 1)
         }
 
-        val locationUpperBoundary = mappings["humidity-to-location"]!!.asSequence()
-            .map { range -> range.dstStart + range.length - 1 }
+        val maxLocation = mappings.last().segments.asSequence()
+            .map { segment -> segment.dst + segment.length - 1 }
             .max()
 
         // TODO: brute force, optimize
-        for (location in 0..locationUpperBoundary) {
-            if (isGood(location, 0)) {
+        for (location in 0..maxLocation) {
+            if (isGood(location, mappings.lastIndex)) {
                 return location
             }
         }
-        return locationUpperBoundary
+        return maxLocation
     }
 
     // test if implementation meets criteria from the description, like:
