66307ee502bc7aec8b0a647c9f6101c5f55975ae
dkoval <koval.dmitriy@gmail.com>
2023-12-07 09:36:04 +0100

    Revert "Solutions to Day07 challenge"
    This reverts commit 63c5969079c17fdf77ff14f52bdd0604655b6ae3.

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -2,12 +2,11 @@ package day05
 
 import println
 import readInputAsString
-import java.util.ArrayDeque
 
 private const val DAY_ID = "05"
 
 fun main() {
-    data class Segment(val src: LongRange, val offset: Long)
+    data class Segment(val src: Long, val dst: Long, val length: Long)
     data class Mapping(val id: String, val segments: List<Segment>)
     data class Input(val seeds: List<Long>, val mappings: List<Mapping>)
 
@@ -37,7 +36,7 @@ fun main() {
                 val segments = mapping.drop(1)
                     .map { nums ->
                         val (dst, src, length) = nums.split(" ").map { it.toLong() }
-                        Segment(src until src + length, dst - src)
+                        Segment(src, dst, length)
                     }
 
                 Mapping(id, segments)
@@ -46,70 +45,66 @@ fun main() {
         return Input(seeds, mappings)
     }
 
-//    fun part1(input: String): Long {
-//        val (seeds, mappings) = parseInput(input)
-//        return seeds.asSequence()
-//            .map { seed ->
-//                var x = seed
-//                for (mapping in mappings) {
-//                    for (segment in mapping.segments) {
-//                        if (x in segment.src until segment.src + segment.length) {
-//                            val offset = x - segment.src
-//                            x = segment.dst + offset
-//                            break
-//                        }
-//                    }
-//                }
-//                x
-//            }
-//            .min()
-//    }
+    fun part1(input: String): Long {
+        val (seeds, mappings) = parseInput(input)
+        return seeds.asSequence()
+            .map { seed ->
+                var x = seed
+                for (mapping in mappings) {
+                    for (segment in mapping.segments) {
+                        if (x in segment.src until segment.src + segment.length) {
+                            val offset = x - segment.src
+                            x = segment.dst + offset
+                            break
+                        }
+                    }
+                }
+                x
+            }
+            .min()
+    }
 
     fun part2(input: String): Long {
         val (seeds, mappings) = parseInput(input)
         val rangeOfSeeds = seeds.chunked(2).map { (start, length) -> start until start + length }
 
-        fun overlap(source: List<LongRange>, mapping: Mapping): List<LongRange> {
-            val ans = mutableListOf<LongRange>()
-            for (range in source) {
-                val q = ArrayDeque<LongRange>().apply { offer(range) }
-                for ((src, offset) in mapping.segments) {
-                    while (!q.isEmpty()) {
-                        val curr = q.peek()
+        fun isGood(x: Long, mappingIndex: Int): Boolean {
+            if (mappingIndex < 0) {
+                // check if x is in range of seeds
+                return rangeOfSeeds.any { x in it }
+            }
 
-                        // overlap?
-                        val first = maxOf(curr.first, src.first)
-                        val last = minOf(curr.last, src.last)
-                        if (first <= last) {
-                            q.poll()
-                            // translate source range -> destination range
-                            ans += first + offset..last + offset
-
-                            if (src.first < first) {
-                                q.offer(curr.first until first)
-                            }
-
-                            if (src.last > last) {
-                                q.offer(last + 1..src.last)
-                            }
-                        }
-                    }
+            // x is the destination in the current mapping
+            var next = x
+            for (segment in mappings[mappingIndex].segments) {
+                if (x in segment.dst until segment.dst + segment.length) {
+                    val offset = x - segment.dst
+                    next = segment.src + offset
+                    break
                 }
             }
-            return ans
+            return isGood(next, mappingIndex - 1)
         }
 
-        return mappings.fold(rangeOfSeeds) { ranges, mapping -> overlap(ranges, mapping) }
-            .println()
-            .minOf { it.first }
+        val maxLocation = mappings.last().segments.asSequence()
+            .map { segment -> segment.dst + segment.length - 1 }
+            .max()
+
+        // TODO: brute force, optimize
+        for (location in 0..maxLocation) {
+            if (isGood(location, mappings.lastIndex)) {
+                return location
+            }
+        }
+        return maxLocation
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsString("day$DAY_ID/Day${DAY_ID}_test")
-    //check(part1(testInput) == 35L)
-    check(part2(testInput).println() == 46L)
+    check(part1(testInput) == 35L)
+    check(part2(testInput) == 46L)
 
     val input = readInputAsString("day$DAY_ID/Day$DAY_ID")
-    //part1(input).println() // answer = 51580674
-    //part2(input).println() // answer = 99751240
+    part1(input).println() // answer = 51580674
+    part2(input).println() // answer = 99751240
 }
