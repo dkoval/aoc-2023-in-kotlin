8d7c16c028ec5c8d7a9274ca7ae5c6b47043df35
dkoval <koval.dmitriy@gmail.com>
2023-12-08 19:10:13 +0100

    Solutions to Day 08 challenge

---
+++ src/day08/Day08.kt
@@ -0,0 +1,85 @@
+package day08
+
+import println
+import readInput
+
+private const val DAY_ID = "08"
+
+fun main() {
+    data class Input(val instructions: String, val nodes: Map<String, Pair<String, String>>)
+
+    fun parseInput(input: List<String>): Input {
+        val instructions = input[0]
+        val nodes = input.drop(2).asSequence()
+            .map { line ->
+                // AAA = (BBB, CCC)
+                val (src, pair) = line.split(" = ")
+                val (left, right) = pair.substring(1, pair.lastIndex).split(", ")
+                src to (left to right)
+            }
+            .toMap()
+
+        return Input(instructions, nodes)
+    }
+
+    fun part1(input: List<String>): Int {
+        val (instructions, nodes) = parseInput(input)
+        val n = instructions.length
+
+        var i = 0
+        var curr = "AAA"
+        var steps = 0
+        while (curr != "ZZZ") {
+            steps++
+            curr = nodes[curr]!!.let { (left, right) -> if (instructions[i++ % n] == 'L') left else right }
+        }
+        return steps
+    }
+
+    fun part2(input: List<String>): Long {
+        val (instructions, nodes) = parseInput(input)
+        val n = instructions.length
+
+        fun getCycleLength(src: String): Int {
+            var i = 0
+            var curr = src
+            while (!curr.endsWith("Z")) {
+                curr = nodes[curr]!!.let { (left, right) -> if (instructions[i++ % n] == 'L') left else right }
+            }
+
+            val start = curr
+            var length = 0
+            do {
+                length++
+                curr = nodes[curr]!!.let { (left, right) -> if (instructions[i++ % n] == 'L') left else right }
+            } while (curr != start)
+            return length
+        }
+
+        fun gcd(x: Long, y: Long): Long {
+            return if (y == 0L) x else gcd(y, x % y)
+        }
+
+        fun lcm(x: Long, y: Long): Long {
+            return x * y / gcd(x, y)
+        }
+
+        return nodes.keys
+            .fold(mutableListOf<Long>()) { acc, src ->
+                if (src.endsWith("A")) {
+                    acc += getCycleLength(src).toLong()
+                }
+                acc
+            }
+            .reduce { x, y -> lcm(x, y) }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    check(part1(readInput("day$DAY_ID/Day${DAY_ID}_part1_test1")) == 2)
+    check(part1(readInput("day$DAY_ID/Day${DAY_ID}_part1_test2")) == 6)
+    check(part2(readInput("day$DAY_ID/Day${DAY_ID}_part2_test")) == 6L)
+
+    val input = readInput("day$DAY_ID/Day$DAY_ID")
+    part1(input).println() // answer = 12599
+    part2(input).println() // answer = 8245452805243
+}
