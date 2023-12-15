5fc5fd7ee846d044ca93f08440233941986e423c
dkoval <koval.dmitriy@gmail.com>
2023-12-15 22:53:24 +0100

    Solutions to Day 15 challenge

---
+++ src/day15/Day15.kt
@@ -0,0 +1,53 @@
+package day15
+
+import println
+import readInputAsString
+
+private const val DAY_ID = "15"
+
+fun main() {
+    fun hash(s: String) = s.fold(0) { acc, c ->
+        var curr = acc
+        curr += c.code
+        curr *= 17
+        curr %= 256
+        curr
+    }
+
+    fun part1(input: String): Int {
+        return input.split(",")
+            .fold(0) { acc, s -> acc + hash(s) }
+    }
+
+    fun part2(input: String): Int {
+        val boxes = input.split(",")
+            .fold(List(256) { mutableMapOf<String, Int>() }) { acc, s ->
+                if (s.last().isDigit()) {
+                    // rn=1
+                    val label = s.dropLast(2)
+                    val x = s.last().digitToInt()
+                    acc[hash(label)][label] = x
+                } else {
+                    // cm-
+                    val label = s.dropLast(1)
+                    acc[hash(label)] -= label
+                }
+                acc
+            }
+
+        return boxes.foldIndexed(0) { boxIndex, acc1, lenses ->
+            acc1 + lenses.values.foldIndexed(0) { lensIndex, acc2, x ->
+                acc2 + (boxIndex + 1) * (lensIndex + 1) * x
+            }
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInputAsString("day$DAY_ID/Day${DAY_ID}_test")
+    check(part1(testInput) == 1320)
+    check(part2(testInput) == 145)
+
+    val input = readInputAsString("day$DAY_ID/Day$DAY_ID")
+    part1(input).println() // answer = 521341
+    part2(input).println() // answer = 252782
+}
